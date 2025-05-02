b489322ad25d3b0576e9774a766791609514559f
Davidfastferrari <daudahassan9825@gmail.com>
2025-05-03 00:41:04 +0100

    try

---
+++ src/calculation/mod.rs
@@ -0,0 +1,7 @@
+//pub mod balancer;
+pub mod uniswap;
+pub mod aerodrome;
+pub mod calculator;
+pub mod curve;
+pub mod maverick;
+pub mod balancer;
---
+++ src/utill/mod.rs
@@ -0,0 +1,21 @@
+
+pub mod calculation;
+pub mod calculator;
+pub mod cache;
+pub mod swap;
+pub mod rgen;
+pub mod tx_sender;
+pub mod stream;
+pub mod simulator;
+pub mod searcher;
+pub mod history_db;
+pub mod quoter;
+pub mod graph;
+pub mod gas_station;
+pub mod filter;
+pub mod events;
+pub mod estimator;
+pub mod constants;
+pub mod bytecode;
+pub mod market_state;
+pub mod ignition;
