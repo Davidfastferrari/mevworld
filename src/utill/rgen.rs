c008e1d2a349f34dc3d3f63cf4e8030b50e2a52c
Davidfastferrari <daudahassan9825@gmail.com>
2025-05-03 07:41:55 +0100

    yc

--- src/utill/rgen.rs
+++ src/utill/rgen.rs
@@ -1,5 +1,6 @@
 use alloy::{primitives::address, providers::ProviderBuilder, sol};
 use std::error::Error;
+use alloy::json_abi::JsonAbi;
 
 // define our flash swap contract {FlashQuoter, FlashSwap};     // #[derive(Debug)]   // #[derive(Debug)]
 
